8e4433a340b6eafdb8558a79cc4a025eaa9c625f
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2023-04-27 15:19:39 +0700

    15h19 27/04/2023

--- code/11.py
+++ code/11.py
@@ -25,3 +25,24 @@ cov = np.mean((age - np.mean(age))*(percent - np.mean(percent)))
 print(cov)
 cor = cov / np.sqrt(var_age * var_percent)
 print(cor)
+
+import scipy.stats as stats
+import numpy as np
+  
+# Make a 5 x 4 table
+dataset = np.array([[4, 13, 17, 11], [4, 6, 9, 12],
+                    [2, 7, 4, 2], [5, 13, 10, 12],
+                    [5, 6, 14, 12]])
+  
+# Finding Chi-squared test statistic, 
+# sample size, and minimum of rows and
+# columns
+X2 = stats.chi2_contingency(dataset, correction=False)[0]
+N = np.sum(dataset)
+minimum_dimension = min(dataset.shape)-1
+  
+# Calculate Cramer's V
+result = np.sqrt((X2/N) / minimum_dimension)
+  
+# Print the result
+print(result)
